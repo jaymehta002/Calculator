b051162a112facb4c4d9bd4bb05d1bf27d36a4cc
jaymehta002 <jaymehta002@gmail.com>
2022-07-13 23:01:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// const arr = ["jay",'\u2665' ,"Yoyo"]
+// arr.sort();
+
+function getRndInteger(){   
+    let x = 0;
+    while(x < 10){
+        x++;
+    }
+}
+document.getElementById('demo').innerHTML = x;
