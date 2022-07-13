42317783aa0999e59a19f1497533c7e934ac1a9d
jaymehta002 <40739838+jaymehta002@users.noreply.github.com>
2022-07-13 23:06:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
-// const arr = ["jay",'\u2665' ,"Yoyo"]
-// arr.sort();
+let equal_pressed = 0;
 
-function getRndInteger(){   
-    let x = 0;
-    while(x < 10){
-        x++;
+let button_input = document.querySelectorAll(".input-button");
+
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+
+window.onload = () => {
+    input.value = "";
+};
+
+button_input.forEach((button_class) => {
+    button_class.addEventListener("click", () => {
+        if(equal_pressed == 1){
+            input.value = "";
+            equal_pressed = 0;
+        }
+        input.value += button_class.value;
+    });
+});
+
+equal.addEventListener("click", () => {
+    equal_pressed = 1;
+    let inp_val = input.value;
+
+    try{
+        let solution = eval(inp_val);
+
+        if(Number.isInteger(solution)){
+            input.value = solution;
+        }
+        else{
+            input.value = toFixed(2);
+        }
+    } catch(err) {
+        alert("Invalid Input");
     }
-}
-document.getElementById('demo').innerHTML = x;
+});
+
+clear.addEventListener("click", () => {
+    input.value = "";
+});
+
+erase.addEventListener("click", () => {
+    input.value = input.value.substr(0, input.value.length - 1);
+});
